2c9ad15954488840a452641a9203255bf65fa168
Abhi Hiremagalur <abhijit@hiremaga.com>
2013-03-23 18:42:08 -0700

    Use subject for clarity

--- spec/wl/cli_spec.rb
+++ spec/wl/cli_spec.rb
@@ -2,11 +2,15 @@ require 'spec_helper'
 
 module Wl
   describe CLI do
+    subject do
+      Wl::CLI.start(args)
+    end
+
     context "by default" do
       let(:args) { [] }
 
       it 'prints helpful information by default' do
-        output = capture(:stdout) { Wl::CLI.start(args) }
+        output = capture(:stdout) { subject }
         output.should include("help [TASK]")
       end
     end
@@ -15,7 +19,7 @@ module Wl
       let(:args) { %w"login --email fake@example.com --password fakepass" }
 
       it 'retrieves a token on behalf of the user' do
-        output = capture(:stdout) { Wl::CLI.start(args) }
+        output = capture(:stdout) { subject }
         output.should include('"name":"fake@example.com"')
       end
     end
